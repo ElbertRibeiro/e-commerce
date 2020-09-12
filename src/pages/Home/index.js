8ee1919af7eb371fb5f25165b15479f1a363b36b
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-09-12 18:36:15 -0300

    update styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
+import './style.css';
 export default function Home() {
   return (
     <div className="Home">
       <header className="Home-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Edit Learn React </p>
       </header>
     </div>
   );
