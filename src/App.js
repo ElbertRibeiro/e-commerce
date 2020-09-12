fd724aab041bccab6c9ecc011fcf671f2fd98e7f
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-09-12 15:36:20 -0300

    delete global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './pages/global.css';
 
 import Routes from './pages/routes';
 
 export default function App() {
   return (
-    <Routes />
+      <Routes />
   );
 }
