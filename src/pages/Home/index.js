59fb95a4e2daa102115bdf8e83ef3c016bdcdbe4
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-09-12 19:10:46 -0300

    update imagens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+
 import './style.css';
+
+import modelo1 from '../../assets/modelo1.jpg';
+import modelo2 from '../../assets/modelo2.jpg';
+
 export default function Home() {
-  return (
-    <div className="Home">
-      <header className="Home-header">
-        <p>
-          Edit Learn React </p>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="headerSite">
+                <h1>Sou + Store</h1>
+                <img src={modelo1} alt="modelo 1" />
+                <img src={modelo2} alt="modelo 2" />
+                <h2>Loginha</h2>
+            </div>
+            <div>
+                <h4>Who We are</h4>
+            </div>
+            <div>
+                <p>produtos em carrocel</p>
+            </div>
+        </>
+    );
 }
