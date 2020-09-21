e723029a41c9ccfb1fdb2e565d5ffb7a922704d5
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-09-21 09:21:14 -0300

    update project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
-
 import './style.css';
 
-import modelo1 from '../../assets/modelo1.jpg';
-import modelo2 from '../../assets/modelo2.jpg';
+import modelo from '../../assets/modelo2.jpg';
 
 export default function Home() {
     return (
         <>
             <div className="headerSite">
                 <h1>Sou + Store</h1>
-                <img src={modelo1} alt="modelo 1" />
-                <img src={modelo2} alt="modelo 2" />
+                
+                <img src={modelo} alt="modelo 2" />
                 <h2>Loginha</h2>
             </div>
+
             <div>
                 <h4>Who We are</h4>
             </div>
+
             <div>
                 <p>produtos em carrocel</p>
+            </div>
+            
+            <div>
+                <h1>Contato</h1>
             </div>
         </>
     );
