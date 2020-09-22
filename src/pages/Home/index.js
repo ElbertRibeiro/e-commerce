8d1d69f010bb450d7452dba637ed98ccef04c467
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-09-22 10:07:13 -0300

    update site

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import './style.css';
-
-import modelo from '../../assets/modelo2.jpg';
+import logo from '../../assets/logo.jpeg';
+import instagram from '../../assets/instagram.svg';
+import whatsapp from '../../assets/whatsapp.svg';
 
 export default function Home() {
     return (
         <>
-            <div className="headerSite">
-                <h1>Sou + Store</h1>
-                
-                <img src={modelo} alt="modelo 2" />
-                <h2>Loginha</h2>
-            </div>
-
-            <div>
-                <h4>Who We are</h4>
-            </div>
-
-            <div>
-                <p>produtos em carrocel</p>
-            </div>
-            
-            <div>
-                <h1>Contato</h1>
+            <div className="header">
+                <img src={logo} alt="logo" className="logo"/> 
+                <img src={instagram} alt="instagram" className="instagram"/>
+                <img src={whatsapp} alt="whatsapp" className="whatsapp"/>
             </div>
         </>
     );
